1fc5deb70df19b4131e35af18c43c8cb35b97315
Amit <amnayak93@gmail.com>
2019-07-04 02:03:00 +0530

    Removed unnecessary code from orderbookapplication

--- Orderbook/src/main/java/com/cs/Orderbook/OrderbookApplication.java
+++ Orderbook/src/main/java/com/cs/Orderbook/OrderbookApplication.java
@@ -1,13 +1,8 @@
 package com.cs.Orderbook;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderbookApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OrderbookApplication.class, args);
-	}
 
 }
